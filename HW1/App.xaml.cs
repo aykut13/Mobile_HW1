b71d03d373b0d3d524a1d184e2873aa454c37878
aykut13 <32415379+aykut13@users.noreply.github.com>
2018-11-04 22:49:07 +0300

    Add files via upload

---
+++ HW1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace HW1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            
+
+            MainPage = new MainPage();
+       
+        }
+    }
+}
